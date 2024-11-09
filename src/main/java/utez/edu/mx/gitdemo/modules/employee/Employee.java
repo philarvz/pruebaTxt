d09d435c83a706c42e502ae89f9942be09162e5b
KatiaAlexandra <20233tn166@utez.com>
2024-11-08 19:53:51 -0600

    Modificación

--- src/main/java/utez/edu/mx/gitdemo/modules/employee/Employee.java
+++ src/main/java/utez/edu/mx/gitdemo/modules/employee/Employee.java
@@ -11,14 +11,45 @@ public class Employee {
     @Column(name = "id")
     private long id;
 
-    @Column(name = "fullName")
-    private String fullName;
+    @Column(name = "Name")
+    private String Name;
+
+    @Column(name = "Lastname")
+    private String lastName;
 
     @ManyToOne
     @JoinColumn(name = "role")
     private Role role;
 
+    public long getId() {
+        return id;
+    }
 
+    public void setId(long id) {
+        this.id = id;
+    }
 
+    public String getName() {
+        return Name;
+    }
 
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
 }
